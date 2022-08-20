8d349a39880c630833039c48b9fc02b88a11488a
ManjiCoder <manjicoder@gmail.com>
2022-08-20 18:27:26 +0530

    HCF & LCM Done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,109 +1,79 @@
 import React, { useState } from "react";
 
 function Main() {
-  const [num1, setNum1] = useState("33");
-  const [num2, setNum2] = useState("3");
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
   const [result, setResult] = useState("");
-  // To showError
-  const showError = () => {
-    console.log("Error");
-  };
 
-  // Function to Calculate HCF
-  const calculateHCF = (e) => {
-    e.preventDefault();
-    // To check or validate userInput
-    if (num1 === 0 || num2 === 0) {
-      showError();
-    } else {
-      const arrContainer = [num1, num2];
-      const sortArrContainer = arrContainer.sort((a, b) => {
-        return a - b;
-      });
-      const max = sortArrContainer[sortArrContainer.length - 1];
-      const min = arrContainer[0];
-      // const max = Math.max(...arrContainer);
-      // const min = Math.min(...arrContainer);
-      const n = Math.round(max / min);
-      console.log("n is", n, min, max);
-      console.log("sort is", sortArrContainer);
-
-      // Function Generating Factors
-      const generateFactors = (num, arr) => {
-        if (num > 0) {
-          arr = [1];
-          for (let i = 1; i <= n; i++) {
-            // console.log(num * i);
-            arr.push(num * i);
-          }
-          console.log(arr);
-          return arr;
-        } else {
-          showError();
-        }
-      };
-      const arr1 = generateFactors(num1);
-      const arr2 = generateFactors(num2);
-
-      const getCommons = (arr1, arr2, commonArr) => {
-        commonArr = arr1.filter((same) => arr2.indexOf(same) !== -1);
-        return commonArr;
-      };
-      const arrCompare = getCommons(arr1, arr2);
-      console.log(arrCompare);
-      const HCF = max % min === 0 ? min : 1;
-      // let a1 = JSON.stringify(arr1);
-      // let a2 = JSON.stringify(arr2);
-      console.log("arrCompare", arrCompare);
-      setResult(
-        <>
-          {/* <div>
-            Factors of {num1} is {a1}
-          </div>
-          <br/>
-          <div>
-            Factors of {num2} is {a2}
-          </div>
-          <br/> */}
-          <span>
-            <strong>HCF</strong> of {num1} & {num2} is <strong>{HCF}.</strong>
-          </span>
-        </>
-      );
-      console.log("HCF is", HCF, arrCompare);
+  // check Number sign & validate
+  const validateNum = (value) => {
+    if (value.length < 8) {
+      if (Math.sign(value) === 1 || Math.sign(value) === 0) {
+        // console.log(value);
+        return value;
+      }
     }
+    return "";
+  };
+  const handleOnChangeNum1 = (e) => {
+    const value = e.target.value;
+    let number1 = validateNum(value);
+    setNum1(number1);
+  };
+  const handleOnChangeNum2 = (e) => {
+    const value = e.target.value;
+    let number2 = validateNum(value);
+    setNum2(number2);
   };
 
-  // Function to Calculate HCF
-  const calculateLCM = (e) => {
-    e.preventDefault();
-    const arrContainer = [num1, num2];
-    const sortedArr = arrContainer.sort((a, b) => {
-      return a - b;
-    });
-    const max = Math.max(...sortedArr);
-    const min = Math.min(...sortedArr);
-    // console.log(min, max);
-
-    const getLCM = (num, arr) => {
-      arr = [];
-      for (let i = num; i <= max; i++) {
+  // HCF
+  const calculateHCF = (n1, n2, arr) => {
+    n1 = Number.parseInt(n1);
+    n2 = Number.parseInt(n2);
+    console.log(n1, n2);
+    arr = [];
+    for (let i = 1; i <= n1 && i <= n2; i++) {
+      // console.log(i);
+      if (n1 % i === 0 && n2 % i === 0) {
         // console.log(i);
         arr.push(i);
+        // console.log(arr);
       }
-      return arr;
-    };
-
-    const arr1 = max % min === 0 ? getLCM(num1) : [num1, num2];
-    const arr2 = max % min === 0 ? getLCM(num2) : [num1, num2];
-    const LCM = max % min === 0 ? max : num1 * num2;
-    console.log(`Mutiples of ${num1}`, arr1);
-    console.log(`Mutiples of ${num2}`, arr2);
-    console.log(LCM);
+    }
+    // setResult(JSON.stringify(arr))
     setResult(
-      <span>
-        <strong>LCM </strong>of {num1} & {num2} is <strong>{LCM}.</strong>
-      </span>
+      <div>
+        <span className="font-bold">HCF</span> of {num1} & {num2} ={" "}
+        <span className="font-bold">{Math.max(...arr)}</span>
+      </div>
+    );
+    console.log(arr);
+    // console.count(arr)
+    // return arr
+  };
+
+  // LCM
+  const calculateLCM = (n1, n2, arr) => {
+    n1 = Number.parseInt(n1);
+    n2 = Number.parseInt(n2);
+    console.log(n1, n2);
+    arr = [];
+    let min = n1 > n2 ? n1 : n2;
+    while (true) {
+      if (min % n1 === 0 && min % n2 === 0) {
+        console.log(min);
+        arr.push(min);
+        break;
+      }
+      min++;
+      // console.log(arr);
+    }
+    console.log(arr);
+    setResult(
+      <div>
+        <span className="font-bold">LCM</span> of {num1} & {num2} ={" "}
+        <span className="font-bold">{Math.max(...arr)}</span>
+      </div>
     );
   };
 
@@ -122,11 +92,7 @@ function Main() {
             name=""
             id="num-1"
             placeholder="Enter a number"
-            onChange={(e) => {
-              Math.sign(e.target.value) === -1
-                ? setNum1(Math.abs(e.target.value))
-                : setNum1(e.target.value);
-            }}
+            onChange={handleOnChangeNum1}
             value={num1}
           />
         </div>
@@ -143,28 +109,38 @@ function Main() {
             name=""
             id="num-2"
             placeholder="Enter an another number"
-            onChange={(e) => {
-              Math.sign(e.target.value) === -1
-                ? setNum2(Math.abs(e.target.value))
-                : setNum2(e.target.value);
-            }}
+            onChange={handleOnChangeNum2}
             value={num2}
           />
         </div>
         <div className="flex flex-col pt-4 space-y-4 m-auto justify-center w-3/4">
           <button
-            className="cursor-pointer text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-5 py-2.5 text-center"
+            className={`${
+              num1 === 0 || num2.length === 0
+                ? "cursor-not-allowed brightness-75"
+                : "cursor-pointer brightness-100"
+            } text-white  bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-5 py-2.5 text-center`}
             type="submit"
             disabled={num1.length === 0 || num2.length === 0}
-            onClick={calculateHCF}
+            onClick={(e) => {
+              e.preventDefault();
+              calculateHCF(num1, num2);
+            }}
           >
             HCF
           </button>
           <button
-            className="cursor-pointer text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-5 py-2.5 text-center"
+            className={`${
+              num1 === 0 || num2.length === 0
+                ? "cursor-not-allowed brightness-75"
+                : "cursor-pointer brightness-100"
+            } text-white  bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-5 py-2.5 text-center`}
             type="submit"
             disabled={num1.length === 0 || num2.length === 0}
-            onClick={calculateLCM}
+            onClick={(e) => {
+              e.preventDefault();
+              calculateLCM(num1, num2);
+            }}
           >
             LCM
           </button>
