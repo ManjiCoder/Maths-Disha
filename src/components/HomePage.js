187dc5cf9e07b8edae87268dbac415faa419b281
ManjiCoder <manjicoder@gmail.com>
2022-08-23 02:22:29 +0530

    Route fixed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,13 +3,18 @@ import { Link } from "react-router-dom";
 
 function HomePage(props) {
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center min-h-screen bg-slate-300 dark:bg-slate-900 gap-7">
+    <div className="flex flex-col flex-wrap justify-start pt-9 items-center min-h-screen bg-slate-300 dark:bg-slate-900 gap-7">
       <span className="p-3 bg-slate-100 border-2 border-gray-900 rounded-md">
         Welcome To {props.title}
       </span>
       <Link to="divisibilityTest">
         <button className="bg-gradient-to-r from-blue-500 to-indigo-700 text-white text-xl p-3 rounded-lg  hover:bg-blue-700 focus:bg-blue-800">
-          Divisibility Test - (2,3,5,9,10)
+          Divisibility Test-(2,3,5,9,10)
+        </button>
+      </Link>
+      <Link to="hcflcm">
+        <button className="px-20 bg-gradient-to-r from-blue-500 to-indigo-700 text-white text-xl p-3 rounded-lg  hover:bg-blue-700 focus:bg-blue-800">
+          HCF / LCM
         </button>
       </Link>
     </div>
