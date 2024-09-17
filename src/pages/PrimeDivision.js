3a69000f3929570bb33ac69433cbf79defd550aa
Manji Coder <manjicoder@gmail.com>
2024-09-17 20:03:15 +0530

    Generate Prime Number done

--- src/pages/PrimeDivision.js
+++ src/pages/PrimeDivision.js
@@ -11,7 +11,13 @@ export default function PrimeDivision() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const primes = generatePrimeNum(num);
-    console.log(primes);
+    console.log(
+      primes,
+      [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+        71, 73, 79, 83, 89, 97,
+      ]
+    );
   };
   return (
     <div className="bg-slate-300 min-h-screen max-h-fit">
--- src/services/primeDivision.js
+++ src/services/primeDivision.js
@@ -1,17 +1,20 @@
 export const generatePrimeNum = (num) => {
   const primes = new Set();
-
-  if (num < 2) {
+  if (num === 2) {
+    primes.add(2);
+  } else if (num < 2) {
     alert("Now allowed!");
-  } else if (num === 2) {
+  } else {
     primes.add(2);
   }
 
   for (let i = 3; i <= num; i++) {
     for (let j = 2; j < i; j++) {
       const isDivisible = i % j === 0;
-      if (isDivisible) break;
-      else if (!isDivisible) primes.add(i);
+      if (isDivisible) {
+        primes.delete(i);
+        break;
+      } else if (!isDivisible) primes.add(i);
     }
   }
 
