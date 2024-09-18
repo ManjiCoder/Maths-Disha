59884e072bdf8804606fcb15f4b5639105973179
Manji Coder <manjicoder@gmail.com>
2024-09-18 14:29:18 +0530

    code review

--- src/pages/PrimeDivision.js
+++ src/pages/PrimeDivision.js
@@ -57,31 +57,35 @@ export default function PrimeDivision() {
           </button>
           <section className="flex flex-col pt-5">
             {steps &&
-              steps.map(({ divisor, divident, reminder }, i) => (
-                <div
-                  key={divisor + reminder}
-                  className="grid grid-cols-2 min-w-[8rem] text-center w-32 mx-auto text-lg font-semibold"
-                >
-                  <span
-                    className={twMerge(
-                      "px-2 border border-l-0 text-blue-700 border-black font-bold italic",
-                      `${i === 0 && "border-t-0"}
-                      ${i === steps.length - 1 && "border-b-0"}`
-                    )}
+              steps.map(({ divisor, divident, reminder }, i) => {
+                const isFirstIdx = i === 0 && "border-t-0";
+                const isLastIdx = i === steps.length - 1 && "border-b-0";
+                return (
+                  <div
+                    key={divisor + reminder}
+                    className="grid grid-cols-2 min-w-[8rem] text-center w-32 mx-auto text-lg font-semibold"
                   >
-                    {divisor}
-                  </span>
-                  <span
-                    className={twMerge(
-                      "px-2 border border-r-0 border-black",
-                      ` ${i === 0 && "border-t-0"}
-                       ${i === steps.length - 1 && "border-b-0"}`
-                    )}
-                  >
-                    {divident}
-                  </span>
-                </div>
-              ))}
+                    <span
+                      className={twMerge(
+                        "px-2 border border-l-0 text-blue-700 border-black font-bold italic",
+                        isFirstIdx,
+                        isLastIdx
+                      )}
+                    >
+                      {divisor}
+                    </span>
+                    <span
+                      className={twMerge(
+                        "px-2 border border-r-0 border-black",
+                        isFirstIdx,
+                        isLastIdx
+                      )}
+                    >
+                      {divident}
+                    </span>
+                  </div>
+                );
+              })}
             <div>
               {divisors && (
                 <div className="font-semibold pt-5 text-lg text-center">
